98a9f5de10d1c2957e20c00f5d4026d0777c461b
donnyrla10 <kokoo1214@naver.com>
2022-10-25 15:46:15 +0900

    add proj

---
+++ HowWeather/HowWeather/HowWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowWeatherApp.swift
+//  HowWeather
+//
+//  Created by 김영선 on 2022/10/25.
+//
+
+import SwiftUI
+
+@main
+struct HowWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
